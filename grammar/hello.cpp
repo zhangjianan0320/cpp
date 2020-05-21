4e9fe16377d27fc15c73d151a140d9478202a875
zjn <zhang_jianan0320@163.com>
2020-05-21 10:08:20 +0800

    1、first program

---
+++ grammar/hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: hello.cpp
+    > Author: zjn
+    > Mail:zjn 
+    > Created Time: 2020年05月21日 星期四 10时04分34秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	std::cout << "hello word\n";
+	return 0;
+}
