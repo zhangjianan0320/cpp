d2a7030ee6c99d0efcb0d95f29ac07e874704260
zjn <zhang_jianan0320@163.com>
2020-05-25 14:05:16 +0800

    1、修正重载的使用和注释

--- 04_reload/reload.cpp
+++ 04_reload/reload.cpp
@@ -23,14 +23,63 @@ class printData
 		};
 };
 
+class Box
+{
+	public:
+		void setLength(int l){
+			length = l;
+		};
+		int getLength(void){
+			return length;
+		};
+		void print(){
+			cout<<"length "<<length<<endl;
+		}
+		Box(int l){
+			length = l;
+			num = ++count;
+			cout<<"creat "<<num<<endl;
+		}
+		Box(){num=++count;};
+		~Box(){
+			cout<<"delete "<<num<<endl;
+		}
+		Box operator+ (const Box& b){
+			//测试发现，在该地方申请的box，在计算完成后便被销毁了，同时参数会全部覆盖返回值，达到将计算结果传递出去的目的，这样操作的内部逻辑不是很清除？
+			Box box;
+			box.setLength( this->length + b.length);
+
+			return box;
+		};
+		static int count;
+	private:
+		int num;
+		int length;
+};
+int Box::count=0;
+void test()
+{
+	Box temp;
+	return ;
+}
 int main(void)
 {
 	printData pd;
-
+/*
+	// 函数重载
 	pd.print(10);
 	//cpp中的小数默认是double的，因此需要强转一下
 	pd.print((float)1.34);
 	pd.print((char *)"hello cpp");
+*/
+//符号重载
+	Box box1(10);
+	Box box2(5);
+	Box box3;
+	box3.print();
+	box3 = box1+box2;
+	cout<<"calc end"<<endl;
+	box3.print();
 
 	return 0;
 }
